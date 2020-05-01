2d3b5df5e627779d44a4062c2af966ad06c87b79
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-01 19:30:11 +0100

    Update day1.py

--- Back End/Beginner/day1.py
+++ Back End/Beginner/day1.py
@@ -1,4 +1,16 @@
-def factor(num):
-    return tuple([i for i in range(1, num) if num % i == 0])
+def factor(num: int) -> tuple:
+    '''
+    A function named factor which uses binary search to reduce the complexity of the algorithm, .
 
-print(factor(10))
+    Parameter: -------
+                num: Integer, the number we want to get it's factors
+
+    Returns: ---------
+                a tuple of integers, where each integers are the factors of the number excluding the numbers itself.
+
+    @author: Babatunde Koiki
+    Created on: 01-05-2020
+    '''
+    return tuple([i for i in range(1, num//2 + 1) if num % i == 0])
+
+print(factor(10)) # (1, 2, 5)
