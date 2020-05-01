6f22a2843e1c2816e400cdcd8f8e7411b4ab9962
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-01 19:52:25 +0100

    Update day1.py

--- Back End/Intermediate/day1.py
+++ Back End/Intermediate/day1.py
@@ -1,1 +1,24 @@
 
+def scrabble(str1: str, str2: str) -> bool:
+    '''
+    A function that checks if it's first parameter has all the characters in the second parameter
+
+    Parameters: -----------
+                str1: str, A text
+                str2: str, A text
+
+    Returns: -----------
+            Returns bool. True if condition is met, otherwise False. 
+
+    @author: Babatunde Koiki
+
+    Created on: 01-01-202
+    '''
+    for i in str2:
+        if i not in str1:
+            return False
+    return True
+
+print(scrabble('jigokuhen','jigen'))
+print(scrabble('i am rain', 'raining'))
+
