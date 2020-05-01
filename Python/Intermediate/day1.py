32dc0a018060339ce8bacac0a765f12464caa122
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-01 20:26:52 +0100

    Update day1.py

--- Python/Intermediate/day1.py
+++ Python/Intermediate/day1.py
@@ -1,1 +1,19 @@
 
+def average(numbers: str) -> int:
+    '''
+    A function that takes in different numbers in a single string and returns the average of the numbers
+
+    Parameters: -------------
+                numbers: str, a text containing numbers seperated by space.
+
+    Returns: --------------
+                an integer, avearge value of the numbers.
+
+    @author: Babatunde Koiki
+    Created on: 01-05-2020
+    '''
+    
+    numbers = [int(number) for number in numbers.split()]
+    return sum(numbers)/len(numbers)
+
+print(average('12 11 2 6 7 10'))
