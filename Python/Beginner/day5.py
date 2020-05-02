17a25042d89254cebc18e05ca68facf36d151a78
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-02 02:05:16 +0100

    Create day5.py

---
+++ Python/Beginner/day5.py
@@ -0,0 +1,11 @@
+def unique(L):
+    '''
+    A function that takes  in a parameter L which is a list.
+    Returns a list of unique numbers of the original list
+    '''
+    return f'Sample List: {L}\nUnique List: {sorted(list(set(L)))}'
+
+
+print(unique([1,2,3,3,3,3,4,5]))
+print(unique([1,2,3,3,3,3,4,5,3,3,3,3,4,5,6,6,11,22,33,44]))
+print(unique([1,2,3,3,3,3,4,5,5,3,3,3,3,4,5,6,6,110,20,19,34]))
