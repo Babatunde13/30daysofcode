5c1c085d1ed505e6bd6529631b47948954ca7f33
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-02 21:39:48 +0100

    Updated day2.py

--- Back End/Intermediate/day2.py
+++ Back End/Intermediate/day2.py
@@ -1,1 +1,27 @@
+def myFunction(num: int) -> int:
+    '''
+    Function that checks the number of times the number in num until it turns to a single number
 
+    Parameter: ------------- 
+                num: int, must be greater than 0
+    
+    Returns: ------------
+                count: int, the number of times *num* can multiply the numbers in it to become a single number.
+    
+    @author: Babatunde Koiki
+    Created on: 02-05-2020
+    '''
+    if num < 0:
+         assert num > 0, 'Number must be positive'
+    count = 0
+    while len(str(num)) > 1:
+        i = 1
+        for j in str(num):
+            i = i * int(j)
+        num = i
+        count += 1
+    return count
+
+print(myFunction(43)) # 2
+print(myFunction(23413)) # 3
+print(myFunction(0)) # 0
