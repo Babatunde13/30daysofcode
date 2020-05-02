11c2c2fb89d02dc3bd12f0d41bb22e5d50264f80
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-02 20:23:46 +0100

    Update day2.py

--- Back End/Beginner/day2.py
+++ Back End/Beginner/day2.py
@@ -1,1 +1,20 @@
+def myFunction(text):
+    '''
+    Function that checks if a text is palindrome.
+    A palindrome text is one that is same as it's reversed text after removing the spaces.
 
+    Parameter: ------------- 
+                text: str, word(s) to check if it's palindrome
+    
+    Returns: ------------
+                True if palindrome, False if otherwise.
+    
+    @author: Babatunde Koiki
+    Created on: 02-05-2020
+    '''
+    if text.replace(' ', '') == text.replace(' ', '')[::-1]:
+        return True
+    return False
+
+print(myFunction('Howdy'))
+print(myFunction('madam'))
