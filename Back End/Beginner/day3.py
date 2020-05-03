48495e62a915e55da6a9a6812fd22ec553cff991
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-03 17:03:32 +0100

    Update day3.py

--- Back End/Beginner/day3.py
+++ Back End/Beginner/day3.py
@@ -1,1 +1,20 @@
+def Santa(numOfSibling: int, numOfSweet: int) -> str:
+  '''
+  A function named santa which checks if second parameter(number of sweet) can shared evenly between fisrt  parameter(number of siblings).
 
+  Parameter: ----------
+            numOfSibling: int, number of siblings that wants to share the sweet
+            
+            numOfSweet: int, number of sweet to be shared evenly by the siblings.
+
+  Returns: ---------------
+            'give away' if sweet can be shared evenly among siblings, otherwise 'eat them yourself'
+        
+  @author: Babatunde Koiki
+  Created on: 03-05-2020
+  '''
+  return 'give away' if numOfSweet % numOfSibling == 0 else 'eat them yourself'
+
+# print(Santa(3, 9))
+# print(Santa(3, 8))
+# print(Santa(4, 7))
