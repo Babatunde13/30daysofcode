acccbc1d3ef12a8af3157cce4f6798b7cf0e2d72
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-03 17:12:41 +0100

    Update day3.py

--- Back End/Intermediate/day3.py
+++ Back End/Intermediate/day3.py
@@ -1,1 +1,9 @@
+def myPackaging(numOfDigits: int, num: int) -> str:
+  '''
+  A functiin named myPackaging which right shitfts a number. It adds zeros to the front of the number.
+
+  Parameters: ----------
+  '''
+  
+  return '0' * (numOfDigits - len(str(num)))+ str(num) if len(str(num)) < numOfDigits else str(num)
 
