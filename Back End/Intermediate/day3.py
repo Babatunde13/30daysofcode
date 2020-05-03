8e68a700ffa36bbba7d9912580d1e26251242592
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-03 17:17:17 +0100

    Update day3.py

--- Back End/Intermediate/day3.py
+++ Back End/Intermediate/day3.py
@@ -1,8 +1,16 @@
 def myPackaging(numOfDigits: int, num: int) -> str:
   '''
-  A functiin named myPackaging which right shitfts a number. It adds zeros to the front of the number.
+  A function named myPackaging which right shifts a number. It adds zeros to the front of the number. The number of zeros to add is the value of first argument - second argument
 
   Parameters: ----------
+            numOfDigits: int, the required size of the number
+            num: int, the number to change it's size
+
+  Returns: --------------
+          A string containing numbers. 
+
+  @author: Babatunde Koiki
+  Created on: 03-05-2020
   '''
   
   return '0' * (numOfDigits - len(str(num)))+ str(num) if len(str(num)) < numOfDigits else str(num)
