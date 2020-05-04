d06716d54459f5d8136d325992556034f5d436e7
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-04 18:05:37 +0100

    Update day4.py

--- Back End/Intermediate/day4.py
+++ Back End/Intermediate/day4.py
@@ -1,1 +1,35 @@
+def isPrime(num: int) -> bool:
+    '''
+    A function that checks if a number is prime.
 
+    Parameters: -----------
+                num: int, Number to check if is prime.
+
+    Return: ------------
+            bool: True if number is prime, otherwise False.
+        
+    @author: Babatunde Koiki
+    Created on: 04-05-2020
+    '''
+        for i in range(2, int(num * 0.5)+1):
+            if num % i == 0: return False
+        return True
+
+def OptimusPrime(num: int) -> list:
+    '''
+    A function that returns a list containing all the prime numbers between 1 and num inclusive.
+
+    Parameters: ------------
+                num, int. Number to end the list.
+
+    Returns: ----------
+            a list of all prime numbers between 1 and num.
+
+    @author: Babatunde Koiki
+    Created on: 04-05-2020
+    '''
+    return [i for i in range(2, num+1) if isPrime(i)]
+
+print(OptimusPrime(7))
+
+        
