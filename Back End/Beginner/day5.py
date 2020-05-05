9aec4567967e424375838cdb4e8e52b24277ab04
Babatunde13 <koikibabatunde14@gmail.com>
2020-05-05 14:49:09 +0100

    Created day5.py

---
+++ Back End/Beginner/day5.py
@@ -0,0 +1,10 @@
+def Triangle_no(num: int) -> bool:
+    '''
+    A function named Triangle_no that checks if a number is a triangle number
+     A triangle number is one 
+    '''
+	return not (8 *num+1) **0.5%1>0
+	
+print(n(6))
+print(n(10))
+print(n(8))
