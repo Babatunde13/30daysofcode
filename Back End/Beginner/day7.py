d160ea5809780e3fcfa9b7d28fdb6c24f6098373
Babatunde13 <koikibabatunde14@gmail.com>
2020-05-07 06:58:53 +0100

    Added day7.py file

---
+++ Back End/Beginner/day7.py
@@ -0,0 +1,15 @@
+def Psum(list1: list, list2: list) -> int:
+    '''
+    A function that takes two lists of same lenght, multiplies the element with same index.
+    
+    Parameters: ---------
+                list1: list 
+                list2: list
+
+    Returns: The sum of element-wise product of two lists
+    '''
+    assert type(list1)==type(list2)==int and all(isinstance(x, int) for x in list1) and all(isinstance(x, int) for x in list2), 'Invalid input'
+    return sum(map(lambda x, y: x*y, list1, list2))
+
+print(Psum([2, 6, 9, 4], [1, 2, 4, 8]))
+print(Psum([1, 2, 'q'], [3, 'b', 1]))
