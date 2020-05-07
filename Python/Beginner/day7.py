171b6ee864f717ba399f9bdbde8dd356e698d03a
Babatunde13 <koikibabatunde14@gmail.com>
2020-05-07 07:02:01 +0100

    Added day7.py file

--- Python/Beginner/day7.py
+++ Python/Beginner/day7.py
@@ -1,5 +1,15 @@
 def averageMultiple(n):
-    return [i for i in range(1, n) if i % 3 == 0  or i % 5 == 0]
+    '''
+    A function that returns the average of multiples of 3 or multiples of 5 from 1 to n without including n.
+
+    Parameter: ----------
+                n: int
+    Returns: int, sum 
+
+    @author: Babatunde Koiki
+    Created on: 07-05-2020
+    '''
+    x = [i for i in range(1, n) if i % 3 == 0  or i % 5 == 0]
     return sum(x)/len(x)
 
 averageMultiple(10)
