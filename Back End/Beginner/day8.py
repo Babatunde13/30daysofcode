e3f7cbd8b882e3643a0ad383a30cba39ad7f92b9
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-05-08 15:02:14 +0100

    Update day8.py

--- Back End/Beginner/day8.py
+++ Back End/Beginner/day8.py
@@ -1,7 +1,8 @@
 def count(num: int) -> int:
     '''
     A function that converts a number to binary and returns the number of 1s present.
+    Parameter: num
     @author: Babatunde Koiki
     Created on: 08-05-2020
     '''
-    return int(bin(num)[2:].count('1'))
+    return 'Invalid, number can't be negative' if num < 0 else int(bin(num)[2:].count('1'))
